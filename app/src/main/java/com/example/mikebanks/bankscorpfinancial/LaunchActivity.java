56c22c7a1d0744cf8c305e2a407ffc99f37759a6
rishikesh.s <rdshinde2000@gmail.com>
2022-11-13 13:18:48 +0530

    Final Commit

---
+++ app/src/main/java/com/example/mikebanks/bankscorpfinancial/LaunchActivity.java
@@ -0,0 +1,100 @@
+package com.example.mikebanks.bankscorpfinancial;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.ShareCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.login_frm_content, new LoginFragment()).commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(getBaseContext(), R.string.account_cancelled, Toast.LENGTH_SHORT).show();
+        super.onBackPressed();
+    }
+    public void removeUpButton() {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+    }
+
+    public void showUpButton() {
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_share) {
+            Intent shareIntent = createShareForecastIntent();
+            startActivity(shareIntent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private Intent createShareForecastIntent(){
+        Intent shareIntent = ShareCompat.IntentBuilder.from(this)
+                .setType("text/plain")
+                .setText("This is a Bank Demo Application as a part of Mobile Computing Mini Project")
+                .getIntent();
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+        return shareIntent;
+    }
+
+    //TODO: NOTE: This code can sometimes run multiple times if login is pressed quickly in succession
+    public void login() {
+        Intent intent = new Intent(getApplicationContext(), DrawerActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void profileCreated(Bundle bundle) {
+
+        Toast.makeText(this, R.string.account_success, Toast.LENGTH_SHORT).show();
+
+        LoginFragment loginFragment = new LoginFragment();
+        loginFragment.setArguments(bundle);
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.login_frm_content, loginFragment).commit();
+    }
+}
